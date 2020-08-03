e03640ec9237704f89aa11e24e26fbc8dc060476
김영호 <hv6kyh@mediagram.co.kr>
2020-08-03 18:45:09 +0900

    웹팩 설정

--- api/v1/stories/list.js
+++ api/v1/stories/list.js
@@ -6,25 +6,29 @@ const params = {
   TableName: process.env.tableName,
 };
 
-module.exports.list = (event, context, callback) => {
-  // fetch all todos from the database
-  dynamoDb.scan(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
-      });
-      return;
-    }
+// module.exports.list = (event, context, callback) => {
+//   // fetch all todos from the database
+//   dynamoDb.scan(params, (error, result) => {
+//     // handle potential errors
+//     if (error) {
+//       console.error(error);
+//       callback(null, {
+//         statusCode: error.statusCode || 501,
+//         headers: { 'Content-Type': 'text/plain' },
+//         body: 'Couldn\'t fetch the todos.',
+//       });
+//       return;
+//     }
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
-  });
-};
+//     // create a response
+//     const response = {
+//       statusCode: 200,
+//       body: JSON.stringify(result.Items),
+//     };
+//     callback(null, response);
+//   });
+// };
+
+export default list2 = (event, context, callback) => {
+  callback(null, 'hi');
+}
